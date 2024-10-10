1a162845cc3fb4664a3259456c37a3eb75264420
Alvise <acarpin32@gmail.com>
2024-10-10 09:11:34 -0400

    added SQL db integration

--- src/main/java/com/alvise1/taskManagementApi/TaskManagementApiApplication.java
+++ src/main/java/com/alvise1/taskManagementApi/TaskManagementApiApplication.java
@@ -3,7 +3,7 @@ package com.alvise1.taskManagementApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.alvise1.taskManagementApi")
 public class TaskManagementApiApplication {
 
 	public static void main(String[] args) {
